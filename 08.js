474bd11f752fbb5c65b6e676cfd3b77f2fd82422
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-16 15:39:57 -0300

    Create 08.js

---
+++ 08.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Digite um número para saber se ele está entre 10 e 50."));
+if ((numero >= 10) && (numero <= 50)){
+    console.log("Seu numero está entre 10 a 50.")
+}
+else{
+    console.log("Seu número não está entre 10 a 50.")
+}
