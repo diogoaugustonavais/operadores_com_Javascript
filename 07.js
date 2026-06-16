e43c6ad5759f0195a8f942ed56d26a0a7bdfdd90
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-16 15:35:55 -0300

    07.js

--- 07.js
+++ 07.js
@@ -2,7 +2,7 @@ let nota = Number(prompt("insira a nota do aluno: "))
 if(nota >= 7){
     alert("aprovado")
 }
-else if((nota <=5 ) || (nota <=6.9)){
+else if((nota >=5 ) && (nota < 7)){
     alert("Recuperaçao")
 }
 else{
