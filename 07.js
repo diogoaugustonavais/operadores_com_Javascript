6b6b066794452048826516c888f1e9bdc6437508
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-16 15:35:19 -0300

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,12 @@
+let nota = Number(prompt("insira a nota do aluno: "))
+if(nota >= 7){
+    alert("aprovado")
+}
+else if((nota <=5 ) || (nota <=6.9)){
+    alert("Recuperaçao")
+}
+else{
+    alert("Reprovado")
+}
+
+
