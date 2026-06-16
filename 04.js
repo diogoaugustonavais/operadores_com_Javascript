802ff07aa18e7dfbf2d570afc3bb3145509664da
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-16 15:22:57 -0300

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,11 @@
+let numero1 = Number(prompt("Digite o dois números para saber qual é maior"));
+let numero2 = Number(prompt("Digite o segundo numero"));
+if (numero1 > numero2){
+    console.log("O número "+numero1+" é maior que o número "+numero2+".")
+} 
+else if(numero1==numero2){
+    console.log("Os dois números são iguais.")
+}
+else {
+    console.log("O número "+ numero2 +" é maior que o número "+ numero1 +".")
+}
