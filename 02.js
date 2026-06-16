c508a71544ea415c3a165bb392f89ff164082922
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-16 15:13:24 -0300

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,7 @@
+let idade = Number(prompt("Digite a sua idade para saber se você tera acesso ou não ao programa"));
+if (idade >= 18){
+    console.log("Acesso liberado: Maior de idade.")
+}
+else {
+    console.log("Acesso negado: menor de idade.")
+}
