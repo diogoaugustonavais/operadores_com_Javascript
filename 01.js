0d5f30c31c564088ca51e0548c48c9c20e224bae
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-16 15:11:50 -0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,13 @@
+let numero = Number(prompt("inisra o numero desejado: "))
+if(numero == 0){
+    alert("seu numero é exatamente zero")
+}
+else if(numero < 0){
+    alert("seu numero é negativo")
+
+}
+else{
+    alert("seu nuemro é positivo")
+}
+
+
